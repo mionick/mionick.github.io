92f067fabd64a4758ade34b0a13a7414773aa61f
mionick <mio_93@hotmail.com>
2014-06-14 23:10:20 -0400

    About section expansion.
    Added "About the site" in the about section.
    
    Also happy with the form finally after the previous commit, though I
    realized the email was hardcoded, so the formFix script wasn't doing
    anything. should be fixed now.
    
    Also added styles for links scattered through text, such as github pages
    and brace.

--- formFix.js
+++ formFix.js
@@ -1,6 +1,4 @@
 <!--
 	var cForm = document.getElementById('contact-form');
-	cForm.action="http://forms.brace.io/mio.clause@gmail.com";
-
-
+	cForm.action="http://forms.brace.io/" +"mio." + "clause" + "@" +"gmail.com";
 -->
